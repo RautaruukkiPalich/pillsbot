335c5b4d1d7ce3e4b2873f12d0a3c14af2c6638c
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-02-14 20:53:32 +0500

    add app ip and host to .env

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import os
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
+
+HOST_ADD = os.environ.get("HOST_ADD")
+HOST_PORT = os.environ.get("HOST_PORT")
