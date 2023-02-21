aa08fe1c3a555853bdfa7c763517cff6f25b4344
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-02-21 22:52:35 +0500

    change "config.set_section_option" to "config.set_main_option"

--- alembic/env.py
+++ alembic/env.py
@@ -1,24 +1,14 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-
 from db.models import Base
-
 from config import DB_NAME, DB_PASS, DB_PORT, DB_USER, DB_HOST
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "DB_PASS", DB_PASS)
-
+config.set_main_option("sqlalchemy.url", f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
