dbac8c66d601e9c54ec38be20f41e2f70b07ceb1
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-02-21 22:50:08 +0500

    add default values

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 load_dotenv()
 
-DB_NAME = os.environ.get('DB_NAME')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
+DB_NAME = os.environ.get('DB_NAME', 'postgres')
+DB_HOST = os.environ.get('DB_HOST', 'database')
+DB_PORT = os.environ.get('DB_PORT', '5432')
+DB_USER = os.environ.get('DB_USER', 'postgres')
+DB_PASS = os.environ.get('DB_PASS', 'postgres')
 
 HOST_ADD = os.environ.get("HOST_ADD")
 HOST_PORT = os.environ.get("HOST_PORT")
