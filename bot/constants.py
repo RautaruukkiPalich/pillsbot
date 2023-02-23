2850ec118aed69094282d40ea5000eca30983eee
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-02-24 02:13:34 +0500

    add defaults

--- bot/constants.py
+++ bot/constants.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HOST_ADD = os.environ.get("HOST_ADD")
-HOST_PORT = os.environ.get("HOST_PORT")
+HOST_ADD = os.environ.get("HOST_ADD", "web")
+HOST_PORT = os.environ.get("HOST_PORT", 8000)
 HOST_URL = f"http://{HOST_ADD}:{HOST_PORT}"
 TOKEN = os.environ.get("TG_TOKEN")
 TIME_4ZONE = {
